fcc3d68eb0502178c6c516276f061006ac0d965d
ChristopherRDunn <crdunn@shaw.ca>
2022-04-05 12:32:29 -0700

    Initial working rcnn

--- duck-hunt/duck_hunt_main.py
+++ duck-hunt/duck_hunt_main.py
@@ -1,13 +1,24 @@
-import argparse
-import gym
-import cv2
+# import some common libraries
+import os, sys
+
+import argparse, gym, pygame, os, json, cv2, random, torch
 import numpy as np
-import pygame
 from concurrent.futures import ThreadPoolExecutor
 
 import ece471_duckhunt as dh 
 from ece471_duckhunt import envs
 from ece471_duckhunt.envs import duckhunt_env
+import detectron2
+from detectron2.utils.logger import setup_logger
+
+# import some common detectron2 utilities
+from roboflow import Roboflow
+from detectron2 import model_zoo
+from detectron2.engine import DefaultPredictor
+from detectron2.engine import DefaultTrainer
+from detectron2.config import get_cfg
+from detectron2.data import MetadataCatalog, DatasetCatalog
+from detectron2.data.datasets import register_coco_instances
 
 from solution import GetLocation 
 
@@ -21,12 +32,56 @@ print(f"OpenGym version: {gym.__version__} (=0.18.0)")
 def noop():
     return [{'coordinate' : 8, 'move_type' : 'relative'}]
 
+def trainModel():
+    cfg = get_cfg()
+    cfg.merge_from_file(
+        "../detectron2_repo/configs/COCO-Detection/faster_rcnn_R_50_FPN_3x.yaml"
+    )
+    cfg.DATASETS.TRAIN = ("Duck",)
+    cfg.DATASETS.TEST = ()  # no metrics implemented for this dataset
+    cfg.DATALOADER.NUM_WORKERS = 2
+    cfg.MODEL.DEVICE = "cpu"
+    cfg.SOLVER.IMS_PER_BATCH = 2
+    cfg.SOLVER.BASE_LR = 0.02
+    cfg.SOLVER.MAX_ITER = (
+        30
+    )  # 300 iterations seems good enough, but you can certainly train longer
+    cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = (
+        128
+    )  # faster, and good enough for this toy dataset
+    cfg.MODEL.ROI_HEADS.NUM_CLASSES = 2  # 2 classes (data, fig, hazelnut)
+
+    modelPath = os.path.join(cfg.OUTPUT_DIR, "model_final.pth")
+    if (not os.path.isfile(modelPath)):
+        cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-Detection/faster_rcnn_R_50_FPN_3x.yaml")  # initialize from model zoo
+        os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
+        trainer = DefaultTrainer(cfg)
+        trainer.resume_or_load(resume=False)
+        trainer.train()
+
+    cfg.MODEL.WEIGHTS = modelPath
+    cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5   # set the testing threshold for this model
+    cfg.DATASETS.TEST = ("Duck", )
+    predictor = DefaultPredictor(cfg)
+    return predictor
+
 """ Here is the main loop for you algorithm """
 def main(args):
-  
+        
     result = {}
     future = None
     executor = ThreadPoolExecutor(max_workers=1)
+
+    setup_logger()
+    datasetPath = "./Duck2-1"
+    rf = Roboflow(api_key="4nPzY2IagI8JDYsMayai")
+    if (not os.path.isdir(datasetPath)):
+        project = rf.workspace("c-d").project("duck2-prqxj")
+        project.version().download("coco")
+    register_coco_instances("Duck", {}, datasetPath + "/train/_annotations.coco.json", datasetPath + "/train/")
+    predictor = trainModel()
+
+
     while True:
         """ 
         Use the `current_frame` from either env.step of env.render
@@ -34,7 +89,7 @@ def main(args):
         
         current_frame : np.ndarray (width, height, 3), np.uint8, RGB
         """
-        current_frame = env.render() 
+        current_frame = env.render()
         
         """
             The game needs to continue while you process the previous image so 
@@ -48,16 +103,16 @@ def main(args):
             have been provided to you.
         """
        
-        if args.move_type == 'manual':
+        # if args.move_type == 'manual':
             #manual mode
-            result = [{"coordinate" : pygame.mouse.get_pos(), 'move_type' : "absolute"}]
-        else:
-            if future is None:
-                result = noop()
-                future = executor.submit(GetLocation, args.move_type, env, current_frame)
-            elif future.done():
-                result = future.result()
-                future = None
+            # result = [{"coordinate" : pygame.mouse.get_pos(), 'move_type' : "absolute"}]
+        # else:
+        if future is None:
+            result = noop()
+            future = executor.submit(GetLocation, "absolute", env, current_frame, predictor)
+        elif future.done():
+            result = future.result()
+            future = None
 
         """
         Pass the current location (and location type) you want the "gun" place.
@@ -119,6 +174,4 @@ if __name__ == "__main__":
                     seed=args.seed,
                     randomize=args.randomize,
                     )
-
-
     main(args)
--- duck-hunt/solution.py
+++ duck-hunt/solution.py
@@ -1,4 +1,5 @@
-import time
+import pygame, random, cv2
+import numpy as np
 
 """
 Replace following with your own algorithm logic
@@ -6,31 +7,28 @@ Replace following with your own algorithm logic
 Two random coordinate generator has been provided for testing purposes.
 Manual mode where you can use your mouse as also been added for testing purposes.
 """
-def GetLocation(move_type, env, current_frame):
-    time.sleep(1) #artificial one second processing time
-    
-    #Use relative coordinates to the current position of the "gun", defined as an integer below
-    if move_type == "relative":
-        """
-        North = 0
-        North-East = 1
-        East = 2
-        South-East = 3
-        South = 4
-        South-West = 5
-        West = 6
-        North-West = 7
-        NOOP = 8
-        """
-        coordinate = env.action_space.sample() 
-    #Use absolute coordinates for the position of the "gun", coordinate space are defined below
-    else:
-        """
-        (x,y) coordinates
-        Upper left = (0,0)
-        Bottom right = (W, H) 
-        """
-        coordinate = env.action_space_abs.sample()
-    
-    return [{'coordinate' : coordinate, 'move_type' : move_type}]
+def GetLocation(move_type, env, current_frame, predictor):
+    shp = current_frame.shape
+    img = cv2.resize(np.array(current_frame), (int(shp[0]/2), int(shp[1]/2)))
+    # img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    # img = img[:, :, np.newaxis]
+    # print("pred")
+    outputs = predictor(current_frame)
+
+    # print(outputs["instances"].pred_boxes.tensor)
+    predictions = outputs["instances"].pred_boxes.tensor.numpy()
+    results = []
+
+    # print(current_frame.shape)
+    if len(predictions) == 0:
+        print("I see nothing")
+    for pred in predictions[:5]:
+        y = int((pred[0]+pred[2])/2)
+        x = int((pred[1]+pred[3])/2)
+        results.append({'coordinate' : (x+160, y), 'move_type' : move_type})
+        results.append({'coordinate' : (x-160, y), 'move_type' : move_type})
+
+    # print(results)
+    # print("Looking at: ", pygame.mouse.get_pos()[1], pygame.mouse.get_pos()[0])
+    return results
 
